abe8f0c9fcc94adf9e2db0345a7cd6586343beab
doston <raximoffdoston@gmail.com>
2025-03-27 16:10:19 +0500

    syncroinizied all codes

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React from "react";
 import { IoEarthOutline } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
 const header = () => {
-    return (
-        <header className='container mx-auto h-20 py-7'>
-            <nav className='flex items-center justify-between '>
-                <h1 className='text-white text-2xl font-normal'>KRAN</h1>
-                <ul className='flex gap-[72px]'>
-                    <li><a className='text-white text-xl font-semibold' href="">Biz haqimizda</a></li>
-                    <li><a className='text-white text-xl font-semibold' href="">Hizmatlar</a></li>
-                    <li><a className='text-white text-xl font-semibold' href="">Kontakt</a></li>
-                </ul>
+  return (
+    <header className="container mx-auto h-20 py-7">
+      <nav className="flex items-center justify-between ">
+        <h1 className="text-white text-2xl font-normal">KRAN</h1>
+        <ul className="flex gap-[72px]">
+          <li>
+            <NavLink to={"/"} className="text-white text-xl font-semibold">
+              Biz haqimizda
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/services"}
+              className="text-white text-xl font-semibold"
+            >
+              Hizmatlar
+            </NavLink>
+          </li>
+          <li>
+            <a className="text-white text-xl font-semibold" href="">
+              Kontakt
+            </a>
+          </li>
+        </ul>
 
-                <div className='flex items-center gap-9'>
-                    <div className='flex items-center gap-1 cursor-pointer'>
-                        <IoEarthOutline color='white' fontSize={24}/>
-                        <h2 className='text-white text-xl font-semibold'>UZ</h2>
-
-                    </div>
-                    <button className='bg-[#FEDF51]  py-2 px-8 rounded-xl text-xl font-semibold'>Bog‘lanish</button>
-                </div>
-
-            </nav>
-          
-        </header>
-    );
+        <div className="flex items-center gap-9">
+          <div className="flex items-center gap-1 cursor-pointer">
+            <IoEarthOutline color="white" fontSize={24} />
+            <h2 className="text-white text-xl font-semibold">UZ</h2>
+          </div>
+          <button className="bg-[#FEDF51]  py-2 px-8 rounded-xl text-xl font-semibold">
+            Bog‘lanish
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default header;
--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import "./Service.css";
+import logoHunday from "../assets/images/logoHunday.svg";
+import HundayKamaz from "../assets/images/hyundaiKamaz.svg";
+import { TfiArrowCircleLeft } from "react-icons/tfi";
+import { TfiArrowCircleRight } from "react-icons/tfi";
 
-const service = () => {
-    return (
-        <div>
-            SEERVICE
+const Service = () => {
+  return (
+    <div className="relative w-full h-screen text-white py-10 flex container mx-auto">
+      <div className="gradient relative right-0 h-[600px] w-[70%] bg-gray-500 text-white py-10 px-20 text-center clip-shape"></div>
+      <div className="absolute left-0 p-10 flex-col items-center h-[600px]">
+        <img src={logoHunday} alt="" />
+        <img src={HundayKamaz} alt="" className="relative bottom-28 left-16" />
+      </div>
+      <div className="absolute top-[-5px] p-10 bottom-0 w-[50%] h-[600px]">
+        <div className="flex gap-10 krone absolute bottom-0">
+          <article>
+            <h1 className="font-normal text-[28px]">300 KG</h1>
+            <p className="mont font-normal text-[20px]">Yuk sig‘imi</p>
+          </article>
+          <article>
+            <h1 className="font-normal text-[28px]">25 METR</h1>
+            <p className="mont font-normal text-[20px]">Yuk sig‘imi</p>
+          </article>
+          <article>
+            <h1 className="font-normal text-[28px]">40 METR</h1>
+            <p className="mont font-normal text-[20px]">Yuk sig‘imi</p>
+          </article>
         </div>
-    );
+      </div>
+      <div className="relative w-[450px] h-[520px] p-[10px] mr-10">
+        <article className="text-center flex-col">
+          <h1 className="krone font-normal text-[24px] ">
+            Avtovishka Daewoo Novus
+          </h1>
+          <p className="mont font-normal text-[16px] leading-6 text-right my-10">
+            Horyong SKY 450SF havo platformalari 45 metrgacha bo'lgan
+            balandlikdagi har qanday qurilish-montaj ishlarini bajarish uchun
+            ajralmas uskunadir. Shuning uchun biz buni xususiy mijozlarga ham,
+            kichik qurilish guruhlariga ham, yirik doimiy mijozlarga ham tavsiya
+            qilamiz.
+          </p>
+        </article>
+        <div className="absolute w-full flex items-center justify-between bottom-0">
+          <div className="flex gap-5">
+            <TfiArrowCircleLeft className="w-[55px] h-[55px]" />
+            <TfiArrowCircleRight className="w-[55px] h-[55px]" />
+          </div>
+          <button className="w-[174px] h-[41px] text-black bg-[#fedf51] rounded-[10px] mont font-semibold text-[20px]">
+            Bog‘lanish
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default service;
+export default Service;
