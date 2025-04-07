c503390ef513d34be7d14bb02a86b25427915cad
doston <raximoffdoston@gmail.com>
2025-04-07 15:38:20 +0500

    container added

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -5,9 +5,9 @@ import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#000000] text-white py-8 px-10 krone container mx-auto text-center">
-      <div className="max-w-screen-2xl">
-        <div className="flex flex-col md:flex-row justify-between items-start gap-8">
+    <div className="w-full bg-[#000000] text-white py-8 px-10 krone text-center">
+      <div className="max-w-screen-2xl container mx-auto">
+        <div className="flex flex-col mx-auto md:flex-row justify-between items-start gap-8">
           {/* Logo Section */}
           <div className="w-full md:w-auto">
             <h1 className="text-2xl font-bold mb-4">KRAN</h1>
